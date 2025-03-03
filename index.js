36049e992ee4cf29915b9aa590b9f37ebbbe90f3
Keyur Mahajan <keyursm2k4@gmail.com>
2025-03-03 17:39:23 +0530

    Intern project

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var port = 4000
+
+app.get('/',(req,res)=>{
+    res.send({
+        message:"Backend project starts now"
+    })
+})
+app.listen(port,()=>{
+    console.log("Our project backend has been started")
+})
