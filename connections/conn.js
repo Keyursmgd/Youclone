a9cdd1aca09d76fd4794ffd9a750c33ae59df33c
Keyur Mahajan <keyursm2k4@gmail.com>
2025-03-05 17:59:00 +0530

    Intern Project

--- connections/conn.js
+++ connections/conn.js
@@ -1,9 +1,33 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+const con = mysql.createConnection({
     host: "localhost",
     user:"root",
     password: "Keyur@12357",
-    database:"youback"
+    database: "youback"
 })
+
+const connDB = async () =>{
+    con.connect(function(err) {
+        if(err){
+            console.error('Error connecting to database: ',err.stack);  
+            process.exit(1);
+        }
+        console.log('Connection established with ID: ',con.threadId);
+        return con;
+    });
+}
+
+const endConnection = async () => {
+    con.end((err) => {
+        if(err) {
+            console.error('Error while closing connection: ', err.stack);
+            process.exit(1);
+        }
+        console.log('Connection Closed Successfully!');
+        return con;
+    });
+}
+
+module.exports = {connDB,endConnection};
 //
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require("express");
+const { connDB } = require(".\\connections\\conn.js");
 var app = express();
 var port = 4000
-
-
 
 app.get('/',(req,res)=>{
     res.send({
         message:"Backend project starts now in node js"
     })
 })
-app.listen(port,()=>{
+app.listen(port, async()=>{
     console.log("Our project backend has been started")
+    const db = await connDB();
 })
