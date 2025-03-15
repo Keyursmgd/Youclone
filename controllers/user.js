17191b183134d770f3dd22dabfeca82924fce809
Keyur Mahajan <keyursm2k4@gmail.com>
2025-03-15 16:27:53 +0530

    API

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,23 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 
-exports.signUp = async(req,res) => {
-    console.log("In signup function")
+exports.signUp = async (req, res) => {
+    // console.log("In signup function")
+    try {
+        const { channelName, userName, about, profilePic,password } = req.body;
+        const isExist = await User.findOne({ userName });
+        console.log(isExist);
+        if (isExist) {
+            res.status(400).json({ error: "Username already exist. Please try again" });
+        } else {
+            let updateP = await bcrypt.hash(password, 10);
+            const use = new User({ channelName, userName, about,profilePic, password: updateP});
+            await use.save();
+            res.status(201).json({ message: "User registered successfully", success: "yes", data: User });
+        }
+
+    } catch (error) {
+
+    }
 }
--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const db = require('./models/index')
 app.use(express.json())
 const AuthRoutes = require("./Routes/user")
 
-
-
 app.use('/auth', AuthRoutes);
+
 db.sequelize.sync().then((req) => {
     app.listen(4000, async()=>{
         console.log("Our project backend has been started");
