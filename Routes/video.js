69102df134786efa58009a5fd004fdabf2928a14
Keyur Mahajan <keyursm2k4@gmail.com>
2025-03-21 17:54:32 +0530

    Get video by id completed

--- Routes/video.js
+++ Routes/video.js
@@ -4,6 +4,8 @@ const vidcontr = require("../controllers/video")
 const auth = require("../middleware/authentication");
 
 router.post('/video',auth,vidcontr.upload)
-
+router.get('/allVideo',vidcontr.getAllVideo)
+router.get('/getVidId/:id',vidcontr.getVidId)
+router.get('/:userId/channel',vidcontr.getAllVideoByUserID)
 
 module.exports = router
--- controllers/video.js
+++ controllers/video.js
@@ -15,3 +15,68 @@ exports.upload = async(req,res) =>{
         res.status(500).json({ error: "Internal server error" });
     }
 }
+
+exports.getAllVideo = async(req,res) =>{
+    try{
+        const vids = await Video.findAll({
+            include: [
+                {
+                    model: db.User, // Ensure this matches your Sequelize model name
+                    attributes: ["channelName", "profilePic", "userName"],
+                },
+            ],
+        });
+        res.status(201).json({success:"true","videos":vids})
+    }catch(error){
+        console.error("Error in getting vid:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+exports.getVidId = async (req,res) => {
+    try {
+        let {id} = req.params
+        const video = await Video.findByPk(id,{
+            include: [
+                {
+                    model: db.User, // Ensure this matches your Sequelize model name
+                    attributes: ["channelName", "profilePic", "userName"],
+                },
+            ],
+        });
+
+        if (!video) {
+            return res.status(404).json({ success: "false", message: "Video not found" });
+        }
+
+        res.status(201).json({success:"true","video": video});
+    } catch (error) {
+        console.error("Error in getting video-id:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+exports.getAllVideoByUserID = async(req,res) =>{
+    try{
+        let {userId} = req.params;
+        const video = await Video.findAll({ 
+            where: { userID: userId } 
+        },{
+            include: [
+                {
+                    model: db.User, // Ensure this matches your Sequelize model name
+                    attributes: ["channelName", "profilePic", "userName"],
+                },
+            ],
+        });
+
+        if (!video) {
+            return res.status(404).json({ success: "false", message: "Video not found" });
+        }
+
+        res.status(201).json({success:"true","video": video});
+    }catch (error) {
+        console.error("Error in getting video-id:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
